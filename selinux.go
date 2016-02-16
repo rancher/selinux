856194cfb1690b7923c4b5518fb03ba830d5ce9d
Josh Curl <hello@joshcurl.com>
2016-02-16 10:52:38 -0800

    Added Getfilecon

--- selinux.go
+++ selinux.go
@@ -15,23 +15,23 @@ package selinux
 // #include <stdlib.h>
 import "C"
 import (
-	"encoding/binary"
-	"crypto/rand"
-	"unsafe"
-	"fmt"
 	"bufio"
-	"regexp"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"strings"
+	"unsafe"
 )
 
 var (
-	assignRegex  = regexp.MustCompile(`^([^=]+)=(.*)$`)
-	mcs_list = make(map[string]bool)
+	assignRegex = regexp.MustCompile(`^([^=]+)=(.*)$`)
+	mcs_list    = make(map[string]bool)
 )
 
-func Matchpathcon(path string, mode int) (string, error) {
+/*func Matchpathcon(path string, mode int) (string, error) {
 	var con C.security_context_t
 	var scon string
 	rc, err := C.matchpathcon(C.CString(path),C.mode_t(mode), &con)
@@ -40,16 +40,27 @@ func Matchpathcon(path string, mode int) (string, error) {
 		C.free(unsafe.Pointer(con))
 	}
 	return scon, err
+}*/
+
+func Getfilecon(path string) (string, int) {
+	var scon string
+	var con *C.char
+	rc, _ := C.lgetfilecon(C.CString(path), &con)
+	if rc > 0 {
+		scon = C.GoString(con)
+		C.free(unsafe.Pointer(con))
+	}
+	return scon, int(rc)
 }
 
-func Setfilecon(path,scon string) (int, error) {
-        rc, err := C.lsetfilecon(C.CString(path),C.CString(scon))
+func Setfilecon(path, scon string) (int, error) {
+	rc, err := C.lsetfilecon(C.CString(path), C.CString(scon))
 	return int(rc), err
 }
 
 func Setexeccon(scon string) (int, error) {
 	var val *C.char
-	if ! Selinux_enabled() {
+	if !Selinux_enabled() {
 		return 0, nil
 	}
 	if scon != "" {
@@ -57,42 +68,43 @@ func Setexeccon(scon string) (int, error) {
 	} else {
 		val = nil
 	}
-        rc, err := C.setexeccon(val)
+	rc, err := C.setexeccon(val)
 	return int(rc), err
 }
 
 type Context struct {
 	con []string
 }
+
 func (c *Context) Set_user(user string) {
-	c.con[0]=user
+	c.con[0] = user
 }
 func (c *Context) Get_user() string {
 	return c.con[0]
 }
 func (c *Context) Set_role(role string) {
-	c.con[1]=role
+	c.con[1] = role
 }
 func (c *Context) Get_role() string {
 	return c.con[1]
 }
 func (c *Context) Set_type(setype string) {
-	c.con[2]=setype
+	c.con[2] = setype
 }
 func (c *Context) Get_type() string {
 	return c.con[2]
 }
 func (c *Context) Set_level(mls string) {
-	c.con[3]=mls
+	c.con[3] = mls
 }
 func (c *Context) Get_level() string {
 	return c.con[3]
 }
-func (c *Context) Get() string{
-	return strings.Join(c.con,":")
+func (c *Context) Get() string {
+	return strings.Join(c.con, ":")
 }
 func (c *Context) Set(scon string) {
-	c.con = strings.SplitN(scon,":",4)
+	c.con = strings.SplitN(scon, ":", 4)
 }
 func New_context(scon string) Context {
 	var con Context
@@ -103,7 +115,7 @@ func New_context(scon string) Context {
 func Is_selinux_enabled() bool {
 	b := C.is_selinux_enabled()
 	if b > 0 {
-		return true;
+		return true
 	}
 	return false
 }
@@ -111,22 +123,22 @@ func Is_selinux_enabled() bool {
 func Selinux_enabled() bool {
 	b := C.is_selinux_enabled()
 	if b > 0 {
-		return true;
+		return true
 	}
 	return false
 }
 
 const (
-	Enforcing = 1
+	Enforcing  = 1
 	Permissive = 0
-	Disabled = -1
+	Disabled   = -1
 )
 
 func Selinux_getenforce() int {
 	return int(C.security_getenforce())
 }
 
-func Selinux_getenforcemode() (int) {
+func Selinux_getenforcemode() int {
 	var enforce C.int
 	C.selinux_getenforcemode(&enforce)
 	return int(enforce)
@@ -141,14 +153,14 @@ func mcs_delete(mcs string) {
 }
 
 func mcs_exists(mcs string) bool {
-	return mcs_list[mcs] 
+	return mcs_list[mcs]
 }
 
 func uniq_mcs(catRange uint32) string {
 	var n uint32
-	var c1,c2 uint32
+	var c1, c2 uint32
 	var mcs string
-	for ;; {
+	for {
 		binary.Read(rand.Reader, binary.LittleEndian, &n)
 		c1 = n % catRange
 		binary.Read(rand.Reader, binary.LittleEndian, &n)
@@ -180,7 +192,7 @@ func free_context(process_label string) {
 func Get_lxc_contexts() (process_label string, file_label string) {
 	var val, key string
 	var bufin *bufio.Reader
-	if ! Selinux_enabled() {
+	if !Selinux_enabled() {
 		return
 	}
 	lxc_path := C.GoString(C.selinux_lxc_contexts_path())
@@ -216,10 +228,10 @@ func Get_lxc_contexts() (process_label string, file_label string) {
 		if groups := assignRegex.FindStringSubmatch(line); groups != nil {
 			key, val = strings.TrimSpace(groups[1]), strings.TrimSpace(groups[2])
 			if key == "process" {
-				process_label = strings.Trim(val,"\"")
+				process_label = strings.Trim(val, "\"")
 			}
 			if key == "file" {
-				file_label = strings.Trim(val,"\"")
+				file_label = strings.Trim(val, "\"")
 			}
 		}
 	}
@@ -235,8 +247,8 @@ exit:
 	return process_label, file_label
 }
 
-func CopyLevel (src, dest string) (string, error) {
-	if ! Selinux_enabled() {
+func CopyLevel(src, dest string) (string, error) {
+	if !Selinux_enabled() {
 		return "", nil
 	}
 	if src == "" {
@@ -256,9 +268,9 @@ func CopyLevel (src, dest string) (string, error) {
 	return tcon.Get(), nil
 }
 
-func Test() {
-	var plabel,flabel string
-	if ! Selinux_enabled() {
+/*func Test() {
+	var plabel, flabel string
+	if !Selinux_enabled() {
 		return
 	}
 
@@ -277,6 +289,6 @@ func Test() {
 	}
 	fmt.Println(Selinux_getenforce())
 	fmt.Println(Selinux_getenforcemode())
-	flabel,_ = Matchpathcon("/home/dwalsh/.emacs", 0)
+	flabel, _ = Matchpathcon("/home/dwalsh/.emacs", 0)
 	fmt.Println(flabel)
-}
+}*/
